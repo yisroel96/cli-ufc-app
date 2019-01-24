4a881bf6c7920db8dabc901b54849d09b97ff8f1
Elijah Cleveland <elicleveland12@gmail.com>
2019-01-24 11:19:45 -0500

    titleize

--- lib/models/comand_line_interface.rb
+++ lib/models/comand_line_interface.rb
@@ -7,9 +7,9 @@ end
 def help
  puts "I accept the following commands:
  - help: displays this message
- - camps: tells you what camp a fighter belongs to
+ - camp: tells you what camp a fighter belongs to
  - weight-class: tells you what weight class a fighter is in
- - camp-spec: tells you about a camp's weight class specialties
+ - camp specialties: tells you about a camp's weight class specialties
  - FIGHT: accepts two fighters and determines if they can fight one another
  - exit: terminates the program"
 end
@@ -24,27 +24,27 @@ def ufc_app
  case command
    when 'help'
      help
-   when 'camps'
+   when 'camp'
      puts "Please enter a fighter's full name"
-     fighter_name = gets.chomp
+     fighter_name = gets.chomp.titleize
      puts Camp.find_by(id: Fighter.find_by(name: fighter_name)[:camp_id])[:camp_name]
      break if (anything_else? == 'please exit')
    when 'weight-class'
      puts "Please enter a fighter's full name"
-     fighter_name = gets.chomp
+     fighter_name = gets.chomp.titleize
      puts WeightClass.find_by(id: Fighter.find_by(name: fighter_name)[:weight_class_id])[:class_name]
      break if (anything_else? == 'please exit')
-   when 'camp-spec'
+   when 'camp specialties'
      puts "Enter a camp name"
-     camp_name = gets.chomp
+     camp_name = gets.chomp.titleize
      camp_specialties(camp_name)
      break if (anything_else? == 'please exit')
    when 'FIGHT'
      puts "Please enter a fighter's full name"
-     fighter_one = gets.chomp
+     fighter_one = gets.chomp.titleize
      fighter_one_weight_class = WeightClass.find_by(id: Fighter.find_by(name: fighter_one)[:weight_class_id])[:class_name]
      puts "Please enter another fighter's full name"
-     fighter_two = gets.chomp
+     fighter_two = gets.chomp.titleize
      fighter_two_weight_class = WeightClass.find_by(id: Fighter.find_by(name: fighter_two)[:weight_class_id])[:class_name]
      if fighter_one == fighter_two
        puts "#{fighter_one} would never fight their twin"
