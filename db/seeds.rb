26ad46a74e4e5cacbc0d0cfa089402832dea0476
Elijah Cleveland <elicleveland12@gmail.com>
2019-11-19 10:15:45 -0500

    updated google scripts to circumvent auth issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@ Fighter.destroy_all
 WeightClass.destroy_all
 Camp.destroy_all
 
-og = RestClient.get('https://script.googleusercontent.com/macros/echo?user_content_key=agz7b2JTBMDYhw-jYgZJJ54X_VNhNWk-se2KK5rq12Dk3ganuH3auz429vq4S_qmKW0emWTJLhkeZ_5mWai2Za3mvtD_Wqjum5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPsHbo7_pNEtT3vLX2y9ZmltLP4YI_OoZHoMd7uFWAK5DhZg0YL3efvxFOT2B0blggegiHP2Ryw5&lib=MagvpSEBNFRfA8U-lFEyQPWiPUb_TAeKa')
+og = RestClient.get('https://script.google.com/macros/s/AKfycbw3zy6ZcIUr_d00ZJ4gJvn7_ZrxWW2wmclY-8_suJeWJlSB3Dln/exec')
 ufc_hash = JSON.parse(og)["user"]
 ufc_hash.each do |fighter|
   Fighter.create!(fighter)
 end
 
-og_wc = RestClient.get('https://script.googleusercontent.com/macros/echo?user_content_key=-g8OQJDRXkfTKI7vZNtz55bTMNiivaVk45m1PREQRU7fiI3L--x6Nt5j9tPtQ3z7HzjMcQDv3F-pTV82kBmdjCUJrBOkWQDlm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBdYbYxA2euhcfJjysFbJHQBpYrD1FIf1l53K0fS57EbpIIjIlZ3KnCeZqmfuGwtHiK1xelrpsbz&lib=MAnlMuLHiss3hP5QJhTzeYGiPUb_TAeKa')
+og_wc = RestClient.get('https://script.google.com/macros/s/AKfycbyHpk1WNjaNGeMFcvgNUq8olBsBhhHwLeWjX8LAneM5TU53yoEv/exec')
 ufc_hash_wc = JSON.parse(og_wc)["user"]
 ufc_hash_wc.each do |wc|
   WeightClass.create!(wc)
 end
 
-og_camp = RestClient.get('https://script.googleusercontent.com/macros/echo?user_content_key=ub0kygIpZZrSlzjq-c9Km4VS0uMXDzRtxWX_OO2x27oeLUQkmrDiaebjM1BkhtNJIb-5gV-AuA5_svYyxs72vyswmmMuxCrEm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnK_KUlfgIhbGQs74yo7biBJapYh2cWz6uhDeDrqg8gqFEpwNMFeYR0kpkBjK3YL8sqOIp7sEUsJ2&lib=MbjqLm5ZlC2DObmZf3rh-_QyfUjkjTl6r')
+og_camp = RestClient.get('https://script.google.com/macros/s/AKfycbx0FRss5QKzS7BDPLL45suaQbAdiwk_dFeQSeGgBQSVxz7KaqE/exec')
 ufc_hash_camp = JSON.parse(og_camp)["user"]
 ufc_hash_camp.each do |camp|
   Camp.create!(camp)
